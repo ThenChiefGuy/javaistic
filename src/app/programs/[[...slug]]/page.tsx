6e987e7663a6aaaa06982ee1c3d0e13d74fcade8
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2025-10-26 11:45:50 +0530

    fix(metadata): Correct URL structure in JSON-LD and metadata generation for pages

--- src/app/programs/[[...slug]]/page.tsx
+++ src/app/programs/[[...slug]]/page.tsx
@@ -31,7 +31,7 @@ export default async function Page(props: {
     },
     mainEntityOfPage: {
       "@type": "WebPage",
-      "@id": `https://javaistic.vercel.app/programs/${page.url}`,
+      "@id": `https://javaistic.vercel.app${page.url}`,
     },
   };
 
@@ -87,7 +87,7 @@ export async function generateMetadata(
       title: `${page.data.title} - Javaistic`,
       description: page.data.description,
       siteName: "Javaistic",
-      url: `https://javaistic.vercel.app/programs/${page.url}`,
+      url: `https://javaistic.vercel.app${page.url}`,
       images: `https://og-javaistic.vercel.app/og?title=${page.data.title}`,
     },
     twitter: {
